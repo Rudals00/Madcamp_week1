6bc7b50eeb5d685b517bf03a0bc96f8735fa1215
2019041703 <rudals9686@naver.com>
2023-06-29 20:38:54 +0900

    imageview

--- app/src/main/java/com/example/madcamp/MainActivity.kt
+++ app/src/main/java/com/example/madcamp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.madcamp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.GridView
+import android.widget.ImageView
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.gridlayout)
+
     }
 }
+
