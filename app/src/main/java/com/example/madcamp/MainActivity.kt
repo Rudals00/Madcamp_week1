92f126b5858fa5071cb66b4082cc19c47e8917ef
Byeongchan Park <pbc1017@kaist.ac.kr>
2023-06-30 15:09:25 +0900

    add three fragments

--- app/src/main/java/com/example/madcamp/MainActivity.kt
+++ app/src/main/java/com/example/madcamp/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.madcamp
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.GridView
-import android.widget.ImageView
-import android.widget.TextView
-
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.madcamp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // 뷰 페이저 어댑터
+    class MyFragmentPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        val fragments = listOf(OneFragment(), TwoFragment(), ThreeFragment())
+        override fun getItemCount(): Int = fragments.size
+        override fun createFragment(position: Int): Fragment = fragments[position]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.gridlayout)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // 툴바 적용
+        setSupportActionBar(binding.toolbar)
+        binding.viewpager.adapter = MyFragmentPagerAdapter(this)
     }
 }
-
