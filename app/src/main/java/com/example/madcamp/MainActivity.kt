184509dbe2587faa6a8b1e826f4d8416b49ec0f7
Byeongchan Park <pbc1017@kaist.ac.kr>
2023-07-01 13:40:03 +0900

    contacts detail

--- app/src/main/java/com/example/madcamp/MainActivity.kt
+++ app/src/main/java/com/example/madcamp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.madcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -29,6 +31,18 @@ class MainActivity : AppCompatActivity() {
         }.attach()
 
         // 툴바 적용
-        setSupportActionBar(binding.toolbar)
+//        setSupportActionBar(binding.toolbar)
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        var initTime = 0L
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            if(System.currentTimeMillis() - initTime > 3000){
+                Toast.makeText(this, "종료하려면 한번 더 누르세요!",
+                    Toast.LENGTH_SHORT).show()
+                initTime = System.currentTimeMillis()
+                return true // 키 이벤트 무시
+            }
+        }
+        return super.onKeyDown(keyCode, event) // 키 이벤트 처리
     }
 }
--- app/src/main/java/com/example/madcamp/OneFragment.kt
+++ app/src/main/java/com/example/madcamp/OneFragment.kt
@@ -6,32 +6,50 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Rect
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
+import androidx.core.view.isGone
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.madcamp.databinding.ActivityMainBinding
 import com.example.madcamp.databinding.FragmentOneBinding
 import com.example.madcamp.databinding.ItemRecyclerview1Binding
 import java.util.Random
 
 class MyViewHolder1(val binding: ItemRecyclerview1Binding): RecyclerView.ViewHolder(binding.root)
 
-class MyAdapter1(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class Person(val name:String, val phone_number:String,val email:String) {
+    var profile_num:Int = 0
+    init {
+        val random = Random()
+        profile_num = random.nextInt(5)
+    }
+}
+
+class MyAdapter1(val datas: MutableList<Person>,val fragmentBinding: FragmentOneBinding): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return MyViewHolder1(ItemRecyclerview1Binding.inflate(
             LayoutInflater.from(parent.context), parent, false))
     }
 
+    fun change_detail(position: Int) {
+        Log.d("chan","clickeditem: ${position}")
+    }
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val binding = (holder as MyViewHolder1).binding
-        binding.itemData.text = datas[position]
-        binding.profileData.text = datas[position].substring(0,1)
-        val random = Random()
-        val num = random.nextInt(5)
+        binding.itemData.text = datas[position].name
+        binding.profileData.text = datas[position].name.substring(0,1)
+
+        val num = datas[position].profile_num
         when(num) {
             0 ->binding.profileData.setBackgroundResource(R.drawable.round_button_1)
             1 ->binding.profileData.setBackgroundResource(R.drawable.round_button_2)
@@ -40,7 +58,10 @@ class MyAdapter1(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerV
             else ->binding.profileData.setBackgroundResource(R.drawable.round_button_5)
         }
 
-
+        holder.binding.itemData.setOnClickListener {
+            change_detail(position)
+            fragmentBinding.detailInfo.visibility=View.VISIBLE
+        }
     }
 
     override fun getItemCount(): Int {
@@ -88,14 +109,18 @@ class OneFragment : Fragment() {
     ): View {
         val binding = FragmentOneBinding.inflate(inflater, container, false)
 
-        val datas = mutableListOf<String>()
+        val datas = mutableListOf<Person>()
         for(i in 1..9){
-            datas.add("item $i")
+            datas.add(Person("item $i","010-0000-0000","dd@gmail.com"))
         }
 
         binding.recyclerview.layoutManager = LinearLayoutManager(activity)
-        binding.recyclerview.adapter = MyAdapter1(datas)
+        binding.recyclerview.adapter = MyAdapter1(datas, binding)
         binding.recyclerview.addItemDecoration(MyDecoration(activity as Context))
+
+        binding.backButton.setOnClickListener {
+            binding.detailInfo.visibility=View.GONE
+        }
 
         return binding.root
     }
