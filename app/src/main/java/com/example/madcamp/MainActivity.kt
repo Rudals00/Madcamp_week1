61f18d298f5e55cc6147e4fe3e34bcebd5746804
2019041703 <rudals9686@naver.com>
2023-07-01 23:08:45 +0900

    imageview

--- app/src/main/java/com/example/madcamp/MainActivity.kt
+++ app/src/main/java/com/example/madcamp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.madcamp
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
--- app/src/main/java/com/example/madcamp/TwoFragment.kt
+++ app/src/main/java/com/example/madcamp/TwoFragment.kt
@@ -23,18 +23,29 @@ import com.example.madcamp.databinding.FragmentTwoBinding
 import com.example.madcamp.databinding.ItemRecyclerview2Binding
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.PopupMenu
+import android.widget.RelativeLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MyViewHolder2(val binding: ItemRecyclerview2Binding) :
-        RecyclerView.ViewHolder(binding.root)
+    RecyclerView.ViewHolder(binding.root)
 class MyAdapter2(val datas: MutableList<Any>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return MyViewHolder2(ItemRecyclerview2Binding.inflate(
@@ -47,58 +58,136 @@ class MyAdapter2(val datas: MutableList<Any>): RecyclerView.Adapter<RecyclerView
         val deviceWidth = displayMetrics.widthPixels
         val deviceHeight = displayMetrics.heightPixels
 
-        // 이미지의 크기와 화면 크기를 비교하여 AlertDialog의 크기를 결정
-        val dialogWidth = if(bitmap.width > deviceWidth) deviceWidth else bitmap.width
-        val dialogHeight = if(bitmap.height > deviceHeight) deviceHeight else bitmap.height
+        // 다이얼로그 크기 재조정
+        val dialogWidth = if (bitmap.width > deviceWidth) deviceWidth else bitmap.width
+        val dialogHeight = if (bitmap.height > deviceHeight) deviceHeight else bitmap.height
 
-        // 커스텀 다이얼로그를 생성
-        val dialog = Dialog(activity).apply {
-            requestWindowFeature(Window.FEATURE_NO_TITLE)
-            setContentView(R.layout.dialog_image)
-            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        // 이미지 비율 계산
+        val imageRatio = bitmap.width.toFloat() / bitmap.height.toFloat()
+
+        if (dialogWidth < deviceWidth && dialogHeight < deviceHeight) {
+            val targetRatio = deviceWidth.toFloat() / deviceHeight.toFloat()
+
+            val layoutParams = RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+
+            // 이미지의 가로 비율과 세로 비율을 비교하여 이미지뷰의 크기를 조정
+            if (imageRatio > targetRatio) {
+                // 이미지의 가로 비율이 더 크면 이미지의 가로 크기를 deviceWidth로 설정하고, 세로 크기는 비율에 맞게 조정
+                val newHeight = (deviceWidth / imageRatio).toInt()
+                if (newHeight > deviceHeight) {
+                    layoutParams.width = (deviceHeight * imageRatio).toInt()
+                    layoutParams.height = deviceHeight
+                } else {
+                    layoutParams.width = deviceWidth
+                    layoutParams.height = newHeight
+                }
+            } else {
+                // 이미지의 세로 비율이 더 크거나 같으면 이미지의 세로 크기를 deviceHeight로 설정하고, 가로 크기는 비율에 맞게 조정
+                val newWidth = (deviceHeight * imageRatio).toInt()
+                if (newWidth > deviceWidth) {
+                    layoutParams.width = deviceWidth
+                    layoutParams.height = (deviceWidth / imageRatio).toInt()
+                } else {
+                    layoutParams.width = newWidth
+                    layoutParams.height = deviceHeight
+                }
+            }
+
+            // 커스텀 다이얼로그를 생성
+            val dialog = Dialog(activity).apply {
+                requestWindowFeature(Window.FEATURE_NO_TITLE)
+                setContentView(R.layout.dialog_image)
+                window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            }
+            val dialogImageView = dialog.findViewById<ImageView>(R.id.dialog_image_view)
+            dialogImageView.layoutParams = layoutParams
+
+            // 이미지뷰의 크기 업데이트
+            dialogImageView.requestLayout()
+
+            // ImageView에 Bitmap 설정
+            dialogImageView.setImageBitmap(bitmap)
+
+            // 다이얼로그 크기 설정
+            dialog.window?.setLayout(dialogWidth, dialogHeight)
+            // 다이얼로그 표시
+            dialog.show()
+        } else {
+            // 이미지의 크기가 화면과 동일하거나 이미지가 화면보다 큰 경우에는 기존 로직을 유지
+            // 커스텀 다이얼로그를 생성
+            val dialog = Dialog(activity).apply {
+                requestWindowFeature(Window.FEATURE_NO_TITLE)
+                setContentView(R.layout.dialog_image)
+                window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            }
+            val dialogImageView = dialog.findViewById<ImageView>(R.id.dialog_image_view)
+
+            // 이미지뷰의 크기 조정
+            if (dialogWidth.toFloat() / dialogHeight.toFloat() > imageRatio) {
+                // 이미지의 가로 비율이 더 작을 경우
+                val newHeight = (dialogWidth / imageRatio).toInt()
+                dialogImageView.layoutParams.width = dialogWidth
+                dialogImageView.layoutParams.height = newHeight
+            } else {
+                // 이미지의 세로 비율이 더 작거나 같을 경우
+                val newWidth = (dialogHeight * imageRatio).toInt()
+                dialogImageView.layoutParams.width = newWidth
+                dialogImageView.layoutParams.height = dialogHeight
+            }
+
+            // 이미지뷰의 크기 업데이트
+            dialogImageView.requestLayout()
+
+            // ImageView에 Bitmap 설정
+            dialogImageView.setImageBitmap(bitmap)
+
+            // 다이얼로그 크기 설정
+            dialog.window?.setLayout(dialogWidth, dialogHeight)
+            // 다이얼로그 표시
+            dialog.show()
         }
-
-        // dialog_image.xml에서 ImageView 찾기
-        val dialogImageView = dialog.findViewById<ImageView>(R.id.dialog_image_view)
-
-        // ImageView에 Bitmap 설정
-        dialogImageView.setImageBitmap(bitmap)
-
-        // 다이얼로그 크기 설정
-        dialog.window?.setLayout(dialogWidth, dialogHeight)
-
-        // 다이얼로그 표시
-        dialog.show()
     }
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val binding = (holder as MyViewHolder2).binding
         when (val item = datas[position]) {
             is Int -> binding.itemData.setImageResource(item)
             is Uri -> Glide.with(holder.itemView.context).load(item).into(binding.itemData)
+            is Bitmap -> binding.itemData.setImageBitmap(item)
             else -> throw IllegalArgumentException("Unexpected data type")
         }
 
         holder.binding.itemData.setOnClickListener {
-            if (datas[position] is Int) {
-                // Local resource image
-                val bitmap = BitmapFactory.decodeResource(it.context.resources, datas[position] as Int)
-                showImageDialog(it.context as Activity, bitmap)
-            } else if (datas[position] is Uri) {
-                // Uri image from gallery
-                val target = object : CustomTarget<Bitmap>() {
-                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                        showImageDialog(it.context as Activity, resource)
-                    }
-
-                    override fun onLoadCleared(placeholder: Drawable?) {
-                        //추상 메소드이기때문에 구현해놓음
-                    }
+            when (val data = datas[position]) {
+                is Int -> {
+                    // Local resource image
+                    val bitmap = BitmapFactory.decodeResource(it.context.resources, data)
+                    showImageDialog(it.context as Activity, bitmap)
                 }
+                is Uri -> {
+                    // Uri image from gallery
+                    val target = object : CustomTarget<Bitmap>() {
+                        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                            showImageDialog(it.context as Activity, resource)
+                        }
 
-                Glide.with(it.context)
-                    .asBitmap()
-                    .load(datas[position])
-                    .into(target)
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                            //추상 메소드이기때문에 구현해놓음
+                        }
+                    }
+
+                    Glide.with(it.context)
+                        .asBitmap()
+                        .load(data)
+                        .into(target)
+                }
+                is Bitmap -> {
+                    // Bitmap image from camera
+                    showImageDialog(it.context as Activity, data)
+                }
+                else -> throw IllegalArgumentException("Unexpected data type")
             }
         }
     }
@@ -125,6 +214,7 @@ class TwoFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -142,18 +232,37 @@ class TwoFragment : Fragment() {
         binding.recyclerview.addItemDecoration(MyDecoration2(activity as Context))
 
         binding.floatingActionButton.setOnClickListener {
-            // Permission check and request
-            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), READ_STORAGE_PERMISSION_CODE)
-            } else {
-                openGallery()
+            val popupMenu = PopupMenu(requireContext(), binding.floatingActionButton)
+            popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.menu_camera -> openCamera()
+                    R.id.menu_gallery -> openGallery()
+                }
+                true
             }
+            popupMenu.show()
         }
 
         return binding.root
     }
 
+    private val cameraResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val imageBitmap: Bitmap? = result.data?.extras?.get("data") as? Bitmap
+            imageBitmap?.let {
+                datas.add(it)
+                (binding.recyclerview.adapter as MyAdapter2).notifyItemInserted(datas.size - 1)
+            }
+        } else {
+            Toast.makeText(requireContext(), "Failed to capture image", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun openCamera() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraResultLauncher.launch(takePictureIntent)
+    }
     private fun openGallery() {
         galleryResultLauncher.launch("image/*")
     }
